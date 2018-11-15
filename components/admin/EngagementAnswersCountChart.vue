988ffc4f4afbb64ff6639956f665e5d645e848c2
Alexandru-Ionut Imbrea <aleximbrea@gmail.com>
2018-11-15 16:31:52 +0000

    Added average mood chart.

---
+++ components/admin/EngagementAnswersCountChart.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="answers-count-chart">
+    <line-chart :chart-data="answersCountChartData" :options="options"></line-chart>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.answers-count-chart {
+    position: relative;
+}
+</style>
+
+
+<script>
+  import LineChart from '@/components/charts/LineChart.js'
+
+  export default {
+    components: {
+        LineChart
+    },
+    data () {
+        return {
+                options: {
+                    responsive: true,
+                    maintainAspectRatio: false
+                }
+        }
+    },
+    props: {
+        answersCountChartData: Object
+    }
+  }
+</script>
